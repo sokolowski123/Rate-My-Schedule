f2afac559cacd11b18ca460a355047590f828300
Alex Sokol <sokola@purdue.edu>
2017-11-21 14:18:17 -0500

    Added routing functionality

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ var routes = require('./routes/routes.js')(app);
 app.set('view engine', 'ejs');
 
 var storage = new MySQLStore(db.con);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+app.set('views', __dirname + '/views');
 app.use(logger('dev'));
 app.use(bodyParser.json);
 app.use(bodyParser.urlencoded());
@@ -32,10 +33,15 @@ module.exports = app;
 var server = app.listen(process.env.PORT || 8080, listen);
 
 function listen() {
-	var host = "ec2-18-216-244-127.us-east-2.compute.amazonaws.com";
-	var port = "8080";
+	//var host = "ec2-18-216-244-127.us-east-2.compute.amazonaws.com";
+	//var port = "8080";
+	
+	//app.listen(host, port);
 	console.log("Listening at http://ec2-18-216-244-127.us-east-2.compute.amazonaws.com");
 
 }
 console.log("Listening on port 8080");
 app.use(express.static('public'));
+
+
+
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,13 @@
 module.exports = function(app) {
-
+	var db = require('../db');
+	var bodyParser = require('body-parser');
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: true }));
+	console.log("outside!");
+	app.get('/', function(req, res) {
+		console.log("Go to main!");
+		res.render('../views/main.ejs');
+	});
 
 
 }
