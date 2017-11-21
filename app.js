32fd53a363d0ca2cfbe72049694877c2c1815e99
Alex Sokol <sokola@purdue.edu>
2017-11-21 13:49:49 -0500

    Added node modules and updated app.js & db.js

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
+var express = require('express');
 var http = require('http');
+var path = require('path');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 
+/*
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
 }).listen(8080); 
+
+*/
+
+var db = require('./db');
+var port = 8080;
+var app = express();
+var routes = require('./routes/routes.js')(app);
+app.set('view engine', 'ejs');
+
+var storage = new MySQLStore(db.con);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded());
+//app.use(app.router);
+
+module.exports = app;
+
+var server = app.listen(process.env.PORT || 8080, listen);
+
+function listen() {
+	var host = "ec2-18-216-244-127.us-east-2.compute.amazonaws.com";
+	var port = "8080";
+	console.log("Listening at http://ec2-18-216-244-127.us-east-2.compute.amazonaws.com");
+
+}
+console.log("Listening on port 8080");
+app.use(express.static('public'));
---
+++ db.js
@@ -0,0 +1,25 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "rmsdb.c6ksrgljhkqt.us-east-2.rds.amazonaws.com",
+	user: "admin252",
+	password: "sokool123",
+	database: "rmsdb" 
+});
+
+con.connect(function(err, data)
+{
+    if (err)
+    {
+        console.log("error connection to db");
+        throw err;
+    }
+    else
+    {
+        console.log("success connection to db");
+        console.log(data);
+    }
+});
+
+module.exports = con;
+
