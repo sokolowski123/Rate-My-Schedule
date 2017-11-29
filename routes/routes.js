5314f973b06e7232e7e7e0228755ec5c64f05e26
jmckesso <jmckesso@purdue.edu>
2017-11-29 15:40:41 -0500

    changed the db calls for write review to work with the update

--- routes/routes.js
+++ routes/routes.js
@@ -65,16 +65,67 @@ module.exports = function(app) {
 	});
 		
 	app.post('/sendreview', function(req, res) {
-                var classname = req.query.classname.toUpperCase();
+                var classname = req.query.classname.toLowerCase();
                 var title = req.query.title;
                 var prof = req.query.prof;
                 var score = req.query.score;
                 var desc = req.query.desc;
                 var query = "insert into review (class, title, prof, score, description) values ('" + classname + "', '" + title + "', '" + prof + "', '" + score + "', '" + desc + "')";
                 var query2 = "select * from classes where number='" + classname + "'";
-		var query3 = "insert into classes (number, name, professor) values ('" + classname + "', \"\", \"\")";
+				var query3 = "insert into classes (number, name, professor) values ('" + classname + "', \"\", \"\")";
+
+				var newQuery = "SELECT cid FROM classes WHERE number='" + classname + "' AND professor='" + prof + "'";
+				console.log(newQuery);
+				db.query(newQuery, function(err, result) {
+					if (result[0] == undefined) {
+						console.log("class doesn't exist");
+						var queryCreate = "INSERT INTO classes(number, name, professor) VALUES ('" + classname + "', \"\", '" + prof + "')";
+						console.log("querycreate " + queryCreate);
+						db.query(queryCreate, function(err, result) {
+							if (err) {
+								throw err;
+							}			
+							else {
+								console.log("class added successfully");
+								db.query(newQuery, function(err, result) {
+									if (result[0] == undefined) {
+										console.log("class still doesn't exist");
+									}
+									else {
+										console.log("have class after inserting");
+										var cid = result[0].cid;
+										var queryReview = "INSERT INTO reviews(class_id, title, score, description) VALUES('" + cid + "', '" + title + "', '" + score + "', '" + desc + "')";
+										console.log( "queryReviw " + queryReview );
+										db.query(queryReview, function(err, result) {
+											if (err) {
+												throw err;
+											}
+											else {
+												console.log("added review");
+											}
+										});
+									}
+
+								});
+							}
+						});
+					}
+					else {
+						var cid = result[0].cid;
+						var queryReview = "INSERT INTO reviews(class_id, title, score, description) VALUES('" + cid + "', '" + title + "', '" + score + "', '" + desc + "')";
+						console.log( "queryReviw " + queryReview );
+						db.query(queryReview, function(err, result) {
+							if (err) {
+								throw err;
+							}
+							else {
+								console.log("added review");
+							}
+						});
+					}
+				});
 		
-		db.query(query2, function(err, result) {
+		/*db.query(query2, function(err, result) {
 			if (result[0] == undefined) {
 				db.query(query3, function(err, result) {
 					if (err) {
@@ -93,7 +144,7 @@ module.exports = function(app) {
 				console.log("Added review to db");
 				res.status(200).json({status: "ok"});
 			}
-		});
+		});*/
 	});
 
 }
